7b38a03eb65bdb5cca107fe9a7c5bf3f8c0da626
angular654 <v.ivahnenko@outlook.com>
2020-06-02 10:05:47 +0300

    delete chat service

--- src/app/check-form-service.service.spec.ts
+++ src/app/check-form-service.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
-import { CheckFormServiceService } from './check-form-service.service';
+import { CheckFormService } from './check-form-service.service';
 
 describe('CheckFormServiceService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: CheckFormServiceService = TestBed.get(CheckFormServiceService);
+    const service: CheckFormService = TestBed.get(CheckFormService);
     expect(service).toBeTruthy();
   });
 });
