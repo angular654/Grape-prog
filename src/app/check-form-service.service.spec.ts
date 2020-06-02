e68c99d88efd4566ae5423234f762b11818fdf07
angular654 <v.ivahnenko@outlook.com>
2020-06-02 14:11:17 +0300

    fix test

--- src/app/check-form-service.service.spec.ts
+++ src/app/check-form-service.service.spec.ts
@@ -4,61 +4,62 @@ import { CheckFormService } from './check-form-service.service';
 
 describe('CheckFormServiceService', () => {
   beforeEach(() => TestBed.configureTestingModule({ providers: [CheckFormService]}));
+
   const user = {
     name:"Max",
     login:"maxsxssd",
     email:"m.sddfd@gmail.com",
     password:"sdsavg378dyq8d7saG"
-};
-
+  };
+  
   it('checkName should return true', () => {
     const service: CheckFormService = TestBed.get(CheckFormService);
-    expect(service.checkName(user.name)).toBeTruthy();
+    expect(service.checkName(user.name)).toBe(true);
   });
 
   it('checkLogin should return true', () => {
     const service: CheckFormService = TestBed.get(CheckFormService);
-    expect(service.checkLogin(user.login)).toBeTruthy();
+    expect(service.checkLogin(user.login)).toBe(true);
   });
 
   it('checkEmail should return true', () => {
     const service: CheckFormService = TestBed.get(CheckFormService);
-    expect(service.checkEmail(user.email)).toBeTruthy();
+    expect(service.checkEmail(user.email)).toBe(true);
   });
 
   it('checkPassword should return true', () => {
     const service: CheckFormService = TestBed.get(CheckFormService);
-    expect(service.checkPassword(user.password)).toBeTruthy();
+    expect(service.checkPassword(user.password)).toBe(true);
   });
   
   it('checkPassword should return true', () => {
     const service: CheckFormService = TestBed.get(CheckFormService);
-    expect(service.checkEmail(user.password)).toBeTruthy();
+    expect(service.checkEmail(user.password)).toBe(true);
   });
   
   it('nameLength should return true', () => {
     const service: CheckFormService = TestBed.get(CheckFormService);
-    expect(service.nameLength(user.name)).toBeTruthy();
+    expect(service.nameLength(user.name)).toBe(true);
   });
 
   it('loginLength should return true', () => {
     const service: CheckFormService = TestBed.get(CheckFormService);
-    expect(service.loginLength(user.login)).toBeTruthy();
+    expect(service.loginLength(user.login)).toBe(true);
   });
 
   it('emailLength should return true', () => {
     const service: CheckFormService = TestBed.get(CheckFormService);
-    expect(service.emailLength(user.email)).toBeTruthy();
+    expect(service.emailLength(user.email)).toBe(true);
   });
 
   it('passwordLength should return true', () => {
     const service: CheckFormService = TestBed.get(CheckFormService);
-    expect(service.passwordLength(user.password)).toBeTruthy();
+    expect(service.passwordLength(user.password)).toBe(true);
   });
 
   it('emailValid should return true', () => {
     const service: CheckFormService = TestBed.get(CheckFormService);
-    expect(service.emailValid(user.email)).toBeTruthy();
+    expect(service.emailValid(user.email)).toBe(true);
   });
   
 });
